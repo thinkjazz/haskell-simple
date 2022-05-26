55d8af9051d4a69f382816d9a4576b31c4e5c970
Bragin Boris <bragin.boris@ya.ru>
2022-05-26 17:23:15 +0300

    Add cond construct

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,13 @@
+{-# LANGUAGE MultiWayIf #-}
+
 module Main where
 
 import Lib
-{-
-Я - сложный многострочный
-комментарий, содержащий
-нечто
-очень важное!
--}
+
 main :: IO ()
 -- А я - скромный однострочный комментарий.
 
-main = putStrLn (checkLocalhost "127.0.0.1")
+main = putStrLn (analyzeGold 750)
 
 
 checkLocalhost :: String -> String
@@ -18,3 +15,11 @@ checkLocalhost ip =
   if ip == "127.0.0.1" || ip == "0.0.0.0"
     then "This is localhost"
     else "This is not localhost"
+
+
+analyzeGold :: Int -> String
+analyzeGold gold
+     | gold == 999 = "Wow, you are rich!"
+     | gold == 750 = "Nice, You have 750 standard!"
+     | gold == 585 = "Not, bad! You are not rich, but you can be rich! Cuz 585 gold is the standard!"
+     | otherwise = "Idk such a standard!"
