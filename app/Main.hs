deba4d8eb4991c6bc8f4e50294edfcb50b8281c0
Bragin Boris <bragin.boris@ya.ru>
2022-05-26 17:46:46 +0300

    Add pattern matching

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ import Lib
 main :: IO ()
 -- А я - скромный однострочный комментарий.
 
-main = putStrLn (analyzeGold 750)
+main = putStrLn (analyzeGold 100)
 
 
 checkLocalhost :: String -> String
@@ -17,9 +17,17 @@ checkLocalhost ip =
     else "This is not localhost"
 
 
-analyzeGold :: Int -> String
-analyzeGold gold
-     | gold == 999 = "Wow, you are rich!"
-     | gold == 750 = "Nice, You have 750 standard!"
-     | gold == 585 = "Not, bad! You are not rich, but you can be rich! Cuz 585 gold is the standard!"
-     | otherwise = "Idk such a standard!"
+-- analyzeGold :: Int -> String
+-- analyzeGold gold
+--      | gold == 999 = "Wow, you are rich!"
+--      | gold == 750 = "Nice, You have 750 standard!"
+--      | gold == 585 = "Not, bad! You are not rich, but you can be rich! Cuz 585 gold is the standard!"
+--      | otherwise = "Idk such a standard!"
+
+
+analyzeGold :: Int -> String -- Одно объявление.
+-- И множество определений...
+analyzeGold 999 = "Wow! 999 standard!"
+analyzeGold 750 = "Great! 750 standard."
+analyzeGold 585 = "Not bad! 585 standard."
+analyzeGold _ = "I don't know such a standard..."
